940677b346803c7186d2e4aaf033f6b4c03092b8
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2025-09-29 10:02:42 -0400

    feat: add method to read String from resource file

--- src/lang/utils/FileReader.java
+++ src/lang/utils/FileReader.java
@@ -1,5 +1,23 @@
 package lang.utils;
 
-public class FileReader {
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
 
+public class FileReader {
+    public static String readSource(String path) throws IOException {
+        byte[] bytes = readBytes(path);
+        return new String(bytes, Charset.defaultCharset());
+    }
+
+    private static byte[] readBytes(String path) throws IOException {
+        String filePath = Objects.requireNonNull(
+                FileReader.class
+                        .getClassLoader()
+                        .getResource(path)
+                        .getPath());
+        return Files.readAllBytes(Paths.get(filePath));
+    }
 }
