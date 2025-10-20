ed5684bc26b264daa5a893f26ac4996563c5b6ed
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2025-10-20 08:46:31 -0400

    feat: add AstPrinter class to parenthesize expressions

---
+++ src/lang/ast/AstPrinter.java
@@ -0,0 +1,19 @@
+package lang.ast;
+
+import lang.expressions.Expression;
+
+public class AstPrinter {
+    public String parenthesize(String name, Expression... expressions) {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+        for (Expression expression : expressions) {
+            builder.append(" ");
+            builder.append(expression.print(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+}
