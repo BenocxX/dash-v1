e467c50294f518bec49c91deef4fc9862a20db8e
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2025-10-27 10:34:03 -0400

    feat: add ability to interpret binary expressions

--- src/lang/expressions/BinaryExpression.java
+++ src/lang/expressions/BinaryExpression.java
@@ -1,5 +1,7 @@
 package lang.expressions;
 
+import lang.interpreter.Interpreter;
+import lang.tokens.TokenType;
 import lang.utils.AstPrinter;
 import lang.tokens.Token;
 
@@ -17,5 +19,35 @@ public class BinaryExpression extends Expression {
     @Override
     public String print(AstPrinter printer) {
         return printer.parenthesize(operator.lexeme, left, right);
+    }
+
+    @Override
+    public Object interpret(Interpreter interpreter) {
+        Object left = this.left.interpret(interpreter);
+        Object right = this.right.interpret(interpreter);
+
+        switch (operator.type) {
+            case TokenType.PLUS:
+                return interpreter.add(left, right);
+            case TokenType.MINUS:
+                return interpreter.substract(left, right);
+            case TokenType.STAR:
+                return interpreter.multiply(left, right);
+            case TokenType.SLASH:
+                return interpreter.divide(left, right);
+            case TokenType.LESS:
+                return interpreter.isLesser(left, right);
+            case TokenType.LESS_EQUAL:
+                return interpreter.isLesserOrEqual(left, right);
+            case TokenType.GREATER:
+                return interpreter.isGreater(left, right);
+            case TokenType.GREATER_EQUAL:
+                return interpreter.isGreaterOrEqual(left, right);
+            case TokenType.EQUAL_EQUAL:
+                return interpreter.isEqual(left, right);
+            case TokenType.BANG_EQUAL:
+                return !interpreter.isEqual(left, right);
+        }
+        return null;
     }
 }
--- src/lang/interpreter/Interpreter.java
+++ src/lang/interpreter/Interpreter.java
@@ -1,6 +1,61 @@
 package lang.interpreter;
 
 public class Interpreter {
+    public Object add(Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) {
+            return (double) left + (double) right;
+        }
+
+        if (left instanceof String && right instanceof String) {
+            return (String) left + (String) right;
+        }
+
+        return null;
+    }
+
+    public double substract(Object left, Object right) {
+        return (double) left - (double) right;
+    }
+
+    public double multiply(Object left, Object right) {
+        return (double) left * (double) right;
+    }
+
+    public double divide(Object left, Object right) {
+        return (double) left / (double) right;
+    }
+
+    public boolean isLesser(Object left, Object right) {
+        return (double) left < (double) right;
+    }
+
+    public boolean isLesserOrEqual(Object left, Object right) {
+        return (double) left <= (double) right;
+    }
+
+    public boolean isGreater(Object left, Object right) {
+        return (double) left > (double) right;
+    }
+
+    public boolean isGreaterOrEqual(Object left, Object right) {
+        return (double) left >= (double) right;
+    }
+
+    public boolean isEqual(Object left, Object right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        // On check si left est null afin d'éviter un NullPointerException
+        // lorsqu'on call "left.equals(right)". Sans le if, ça pourrait
+        // essayer de call "null.equals(right)".
+        if (left == null) {
+            return false;
+        }
+
+        return left.equals(right);
+    }
+
     // Seulement "nil" et "false" sont des valeurs "falsy", tout le reste est "truthy"
     public boolean isTruthy(Object value) {
         if (value == null) {
